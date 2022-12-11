755810d958533e5cacf74c5fe07f4fbedefe412e
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-11 19:43:09 +0530

    first

---
+++ src/main/java/com/example/nestemployee_backend/NestemployeeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.nestemployee_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestemployeeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestemployeeBackendApplication.class, args);
+	}
+
+}

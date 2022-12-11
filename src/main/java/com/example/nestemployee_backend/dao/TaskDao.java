391ed3721448f1b8afb7bd3b817bec5c9ef3abb5
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-11 23:55:44 +0530

    view task

--- src/main/java/com/example/nestemployee_backend/dao/TaskDao.java
+++ src/main/java/com/example/nestemployee_backend/dao/TaskDao.java
@@ -1,7 +1,14 @@
 package com.example.nestemployee_backend.dao;
 
 import com.example.nestemployee_backend.model.Task;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Map;
+
 public interface TaskDao extends CrudRepository<Task,Integer> {
+
+    @Query(value = "SELECT e.`address`, e.`email`, e.`empcode`, e.`name`, e.`phno`,t.`task`,t.`status` FROM `employees` e JOIN task t ON e.empcode=t.empcode")
+    List<Map<String,String>> viewall();
 }

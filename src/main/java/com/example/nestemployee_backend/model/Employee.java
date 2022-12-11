2cefee279b1d8e60a94dd707adececfaa164e4bc
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-11 20:31:09 +0530

    add employee

---
+++ src/main/java/com/example/nestemployee_backend/model/Employee.java
@@ -0,0 +1,99 @@
+package com.example.nestemployee_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String empcode;
+    private String address;
+    private String email;
+    private String phno;
+    private String username;
+    private String password;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String name, String empcode, String address, String email, String phno, String username, String password) {
+        this.id = id;
+        this.name = name;
+        this.empcode = empcode;
+        this.address = address;
+        this.email = email;
+        this.phno = phno;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmpcode() {
+        return empcode;
+    }
+
+    public void setEmpcode(String empcode) {
+        this.empcode = empcode;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhno() {
+        return phno;
+    }
+
+    public void setPhno(String phno) {
+        this.phno = phno;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
